ff1416d8884963180a7db2da2f7ef3c3a8a50db2
Akshayjosephtomy <akshayjt007@gmail.com>
2022-10-10 21:36:19 +0530

    todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodolistComponent } from './todolist/todolist.component';
+
+const todoRoutes:Routes[
+  {
+    path:"",component:TodolistComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodolistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(todoRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
